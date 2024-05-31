a7786944eac8132a9991c8b3d620bc34ab5c074f
Kaushal <83158302+KaushalPanpaliya@users.noreply.github.com>
2024-05-31 22:45:21 +0530

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -37,25 +37,3 @@ func (bc *Blockchain) AddBlock(data string) {
 	bc.blocks = append(bc.blocks, newBlock)
 }
 
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{}) // creates a Genesis block using malloc
-}
-
-func NewBlockchain() *Blockchain {
-	var blockchain = Blockchain{[]*Block{NewGenesisBlock()}}
-	return &blockchain
-}
-
-func main() {
-	var bc = NewBlockchain()
-
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
-}
